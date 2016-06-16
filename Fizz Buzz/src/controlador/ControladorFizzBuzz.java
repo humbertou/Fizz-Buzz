ccc95021cf4937b429815662c2e62c3a61b31e70
humbertou <humbertou.va@gmail.com>
2016-06-16 00:30:20 -0600

    En esta version se termina la parte del controlador. Se crea un método main que pone en true la visibilidad de la ventana. Se usan estándares de clean code.

---
+++ Fizz Buzz/src/controlador/ControladorFizzBuzz.java
@@ -0,0 +1,38 @@
+package controlador;
+
+/**
+ * Esta clase es la controla la lógica del Fizz Buzz y la interfaz de este.
+ * Llama a los métodos de la clase FizzBuzz, para que puedan ser utilizados en 
+ * la interfaz, sin que esta "se de cuenta" que existe una lógica.
+ * 
+ * @version 15 de junio 2016 
+ * @author Humberto
+ */
+
+import modelo.FizzBuzz;
+import vista.VentanaFizzBuzz;
+
+public class ControladorFizzBuzz {
+    private final FizzBuzz fizzbuzz;
+    private static final VentanaFizzBuzz ventanaFizzBuzz = new VentanaFizzBuzz();
+    
+    public ControladorFizzBuzz(){
+       this.fizzbuzz = new FizzBuzz();
+       
+    }
+    
+    /**
+     * Este método llama al método de listar números de la parte de la lógica
+     * del fizzbuzz
+     * 
+     * @return String[] Devuelve la lista de números en un string
+     */
+    public String[] listarNumeros(){
+        return fizzbuzz.enlistarNumero();
+    }
+    
+     public static void main(String args[]) {
+        ventanaFizzBuzz.setVisible(true);
+    }
+    
+}
